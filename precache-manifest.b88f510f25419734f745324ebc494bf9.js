ae646d824a90a4ef1b0bbcad64e047404ed817af
LanguageXange <hlin0017@seattlecentral.edu>
2020-07-14 10:27:33 +0800

    Updates

---
+++ precache-manifest.b88f510f25419734f745324ebc494bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6ad89fd0875a1791c8e3996c8fd0bc",
+    "url": "/reacthook-tictactoe/index.html"
+  },
+  {
+    "revision": "1d34379ae4a88f494b82",
+    "url": "/reacthook-tictactoe/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dd03bb2ef565495d46ef",
+    "url": "/reacthook-tictactoe/static/js/2.71c1c761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthook-tictactoe/static/js/2.71c1c761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d34379ae4a88f494b82",
+    "url": "/reacthook-tictactoe/static/js/main.7ea5297d.chunk.js"
+  },
+  {
+    "revision": "3b9e346c1bc493685d06",
+    "url": "/reacthook-tictactoe/static/js/runtime-main.f12dbe01.js"
+  }
+]);
